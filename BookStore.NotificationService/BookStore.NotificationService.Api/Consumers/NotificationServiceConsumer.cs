2e42fb674a96decac8fa7a846e5751c36790305a
nghia46 <nghialoe46@gmail.com>
2024-10-05 12:40:10 +0700

    update

--- BookStore.NotificationService/BookStore.NotificationService.Api/Consumers/NotificationServiceConsumer.cs
+++ BookStore.NotificationService/BookStore.NotificationService.Api/Consumers/NotificationServiceConsumer.cs
@@ -9,6 +9,7 @@ public class NotificationServiceConsumer : IConsumer<BorrowingRequest>
     public async Task Consume(ConsumeContext<BorrowingRequest> context)
     {
         var book = context.Message;
+
         await Console.Out.WriteLineAsync($"Borrowing book event received: {book.ToJson()}");
     }
 }
